52115d31dff777d72a8359a4e5be2ade4e97b59b
Daniel Jimenez <danieljimenezcanales@gmail.com>
2020-11-26 00:06:43 -0500

    update gradle

--- app/src/main/java/pe/edu/upc/bodeguin/data/persistance/database/AppDatabase.kt
+++ app/src/main/java/pe/edu/upc/bodeguin/data/persistance/database/AppDatabase.kt
@@ -1,9 +1,9 @@
 package pe.edu.upc.bodeguin.data.persistance.database
 
 import android.content.Context
-import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.Database
 import pe.edu.upc.bodeguin.data.persistance.dao.CartDao
 import pe.edu.upc.bodeguin.data.persistance.dao.UserDao
 import pe.edu.upc.bodeguin.data.persistance.model.Cart
